f00e1a3f8f18093e56a42092230cc05c50af6513
YSDB <lixinyi54068@gmail.com>
2020-04-17 09:23:41 +0800

    Update Recursion.py

--- Binary tree/Pre-order traversal/Recursion.py
+++ Binary tree/Pre-order traversal/Recursion.py
@@ -1,1 +1,14 @@
 class TreeNode:
+  def __init__(self,x):
+    self.val = x
+    self.left = None
+    self.right = None
+   
+def mid_travel(root):
+  if not root:
+    return
+  mid_travel(root.left)
+  root.val
+  mid_travel(root.right)
+  return 
+    
